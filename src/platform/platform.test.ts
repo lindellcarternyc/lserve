39536568de3aac3dc6e31001103835660770d562
lindellcarternyc <lindellcarternyc@gmail.com>
2022-08-03 13:21:14 -0400

    adds support for configuring port

--- src/platform/platform.test.ts
+++ src/platform/platform.test.ts
@@ -1,5 +1,7 @@
 import { Platform } from './platform'
 import request from 'supertest'
+import { Container } from '../container/container'
+import { Config } from '../decorators/config/config'
 
 describe(Platform, () => {
   it('exists', () => {
@@ -7,9 +9,39 @@ describe(Platform, () => {
   })
 
   describe('#bootstrap', () => {
-    it('creates a default server', () => {
+    it('creates a default server', async () => {
       const server = Platform.bootstrap()
+      await server.start()
+      
       expect(server).toBeDefined()
+      expect(server.address().port).toBe(1234)
+      server.stop()
+    })
+    
+    it('creates a server using a provided class', async () => {
+      class Server {
+        port = 6969
+      }
+
+      Container.set(Server)
+      const server =  Platform.bootstrap(Server)
+      await server.start()
+      expect(server.address().port).toBe(6969)
+      server.stop()
+    })
+
+    it('uses the metadata provided by the config decorator', async () => {
+      @Config({
+        port: 8989
+      })
+      class S {
+        port = 9999
+      }
+
+      const server = Platform.bootstrap(S)
+      await server.start()
+      expect(server.address().port).toBe(8989)
+      server.stop()
     })
 
     describe('#start', () => {
@@ -18,8 +50,8 @@ describe(Platform, () => {
         const fn = jest.fn()
         await server.start(fn)
         expect(fn).toBeCalledTimes(1)
+        expect(server.address().port).toBe(1234)
         server.stop()
-        // done()
       })
 
       it('gets requests', (done) => {
--- src/platform/platform.ts
+++ src/platform/platform.ts
@@ -1,26 +1,42 @@
 import express from 'express'
-import { Server } from 'http'
+import { Container } from '../container/container'
+import { Callable } from '../types/callable'
 
 interface PlatformServer {
   start(fn?: (app: express.Application) => void): Promise<void>
   stop(fn?: () => void): void
   app: express.Application
+  address(): {
+    port: number
+  }
+}
+
+interface PlatformConfig {
+  port: number
+}
+
+const DefaultConfig: PlatformConfig = {
+  port: 1234
 }
 
 export class Platform {
   private static readonly app: express.Application = express()
-  private static server: Server
+  private static server: ReturnType<express.Application['listen']>
 
-  
-  static bootstrap(): PlatformServer {
+  static bootstrap(): PlatformServer
+  static bootstrap(module: Callable<unknown>): PlatformServer
+  static bootstrap(module?: Callable<unknown>): PlatformServer {
     Platform.app.get('/', (_, res) => {
       res.send('Hello World!')
     })
+
+    const config = Platform.getConfig(module)
+
     return {
       start(fn?) {
         return new Promise((res, rej) => {
           try {
-            Platform.server = Platform.app.listen(1234, () => {
+            Platform.server = Platform.app.listen(config.port as number, () => {
               if (fn) fn(Platform.app)
               res()
             })
@@ -34,7 +50,45 @@ export class Platform {
         Platform.server.close()
         if (fn) return fn()
       },
-      app: Platform.app
+      app: Platform.app,
+      address() {
+        return {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          port: ((Platform.server.address() as any).port)
+        }
+      }
     }
   }
+
+  private static getConfig(module: Callable<unknown> | undefined): PlatformConfig {
+    if (!module) return DefaultConfig
+
+    const serverOptions = Reflect.getMetadata('options', module)
+    const serverValue = Container.get(module)
+    
+    const config = merge(serverOptions, serverValue as Partial<PlatformConfig>, DefaultConfig)
+
+    return config
+  }
+}
+
+const merge = <T>(obj1: Partial<T>, obj2: Partial<T>, defaults: T): T => {
+  const result = (Object.keys(defaults) as (keyof T)[]).reduce((res, key) => {
+    if (obj1 && obj1[key]) {
+      return {
+        ...res,
+        [key]: obj1[key]
+      }
+    } else if (obj2 && obj2[key]) {
+      return {
+        ...res,
+        [key]: obj2
+      }
+    }
+    return {
+      ...res,
+      [key]: defaults[key]
+    }
+  }, { } as T)
+  return result
 }
